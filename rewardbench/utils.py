824c2dc44fd9e7b6a9fbe05354420f2d91eb7a18
Wilson Tandya <68466220+WilsonTandya@users.noreply.github.com>
2025-05-09 21:57:02 +0800

    Rename 'category' to 'subset' so RewardBench logic works as-is

--- rewardbench/utils.py
+++ rewardbench/utils.py
@@ -327,6 +327,8 @@ def load_eval_dataset(
     if core_set:
         #raw_dataset = load_dataset(CORE_EVAL_SET, split="filtered")
         raw_dataset = load_dataset(CORE_EVAL_SET, name="ind_Latn", split="test")
+        raw_dataset = raw_dataset.rename_column("category", "subset") # Rename 'category' to 'subset' so RewardBench logic works as-is
+
 
     else:
         raw_dataset = load_dataset(EXTRA_PREF_SETS)
