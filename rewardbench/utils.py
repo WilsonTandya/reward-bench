b8204b592d8a5314d5caa36b7da748badc12f904
Wilson Tandya <68466220+WilsonTandya@users.noreply.github.com>
2025-05-25 14:30:42 +0800

    kor_Hang

--- rewardbench/utils.py
+++ rewardbench/utils.py
@@ -328,7 +328,7 @@ def load_eval_dataset(
     if core_set:
         # raw_dataset = load_dataset(CORE_EVAL_SET, split="filtered")
         
-        dataset_name = "hin_Deva"
+        dataset_name = "kor_Hang"
         raw_dataset = load_dataset(CORE_EVAL_SET, name=dataset_name, split="test")
         raw_dataset = raw_dataset.rename_column("category", "subset") # Rename 'category' to 'subset' so RewardBench logic works as-is
 
